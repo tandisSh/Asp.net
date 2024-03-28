d7da7c8ddb5ea5578cd9ca3257dd5b90f05b55e2
tandisSh <tandis00shojaee@gmail.com>
2024-03-28 22:12:35 +0330

    edit

--- elinor/elinor/Program.cs
+++ elinor/elinor/Program.cs
@@ -2,6 +2,7 @@ using elinor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -9,3 +10,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
