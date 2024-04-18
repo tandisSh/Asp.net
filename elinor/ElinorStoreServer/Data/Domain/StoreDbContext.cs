c0f9ecfc1118c5f451939ffac7384629898e7d5c
tandisSh <tandis00shojaee@gmail.com>
2024-04-19 01:00:22 +0330

    add storeServer

---
+++ elinor/ElinorStoreServer/Data/Domain/StoreDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ElinorStoreServer.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+
+namespace ElinorStoreServer.Data.Domain
+{
+    public class StoreDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public StoreDbContext(DbContextOptions<StoreDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
